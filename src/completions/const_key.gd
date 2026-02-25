fcc487733b18341d29f4a4d885dd0fbb74e81c62
brohd <brohdie_larocque@hotmail.com>
2026-02-24 21:16:06 -0800

    update const key

--- src/completions/const_key.gd
+++ src/completions/const_key.gd
@@ -8,7 +8,7 @@ func _get_completion_settings() -> Dictionary:
 		"priority": 100,
 	}
 
-func _on_editor_script_changed(script):
+func _on_editor_script_changed(_script):
 	pass
 
 
@@ -17,7 +17,6 @@ func _on_code_completion_requested(script_editor:CodeEdit) -> bool:
 	if state != State.STRING:
 		return false
 	
-	
 	var line = script_editor.get_line(script_editor.get_caret_line())
 	var stripped = line.strip_edges()
 	if stripped.begins_with("const"):
@@ -25,10 +24,44 @@ func _on_code_completion_requested(script_editor:CodeEdit) -> bool:
 		if const_data == null:
 			return false
 		var const_name = const_data[0] as String
-		var upper = const_name.to_upper()
-		var snake_case = const_name.to_snake_case()
-		for case in [upper, snake_case]:
-			var option = get_code_complete_dict(CodeEdit.CodeCompletionKind.KIND_PLAIN_TEXT, case, case, "const")
+		var to_convert = [const_name]
+		var access_name = get_current_class()
+		
+		var script:GDScript = get_current_script()
+		var script_name = script.get_global_name()
+		if script_name == "":
+			script_name = script.resource_path.get_file().get_basename().to_pascal_case()
+		
+		if access_name != "":
+			var full_access = access_name.path_join(const_name)
+			var full_script = script_name.path_join(full_access)
+			full_access = full_access.replace("/", ".")
+			full_script = full_script.replace("/", ".")
+			to_convert.append(full_access)
+			to_convert.append(full_script)
+		else:
+			var full_script = script_name.path_join(const_name)
+			full_script = full_script.replace("/", ".")
+			to_convert.append(full_script)
+		
+		var options_to_add = []
+		for path:String in to_convert:
+			var snake = path.to_snake_case()
+			options_to_add.append(snake)
+			options_to_add.append(snake.to_upper())
+			#options_to_add.append(path)
+			
+			#options_to_add.append(path.to_pascal_case())
+			#options_to_add.append(path.to_camel_case())
+			#options_to_add.append(path.to_kebab_case())
+		
+		
+		for case in options_to_add:
+			var location = CodeEdit.LOCATION_LOCAL
+			if case.contains("."):
+				location = CodeEdit.LOCATION_OTHER_USER_CODE
+			
+			var option = get_code_complete_dict(CodeEdit.CodeCompletionKind.KIND_PLAIN_TEXT, case, case, "const", null, location)
 			add_completion_option(script_editor, option)
 		return true
 	
